006d6772b95d5770fb4ea364e67935a43159f712
sergey <alles1028@gmail.com>
2022-10-30 18:37:06 +0300

    Еще исправления

--- main-service/src/main/java/ru/practicum/explore/client/BaseClient.java
+++ main-service/src/main/java/ru/practicum/explore/client/BaseClient.java
@@ -2,12 +2,11 @@ package ru.practicum.explore.client;
 
 import io.micrometer.core.lang.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -31,7 +30,7 @@ public class BaseClient {
                                                           String path,
                                                           @Nullable Map<String, Object> parameters,
                                                           @Nullable T body) {
-        HttpEntity<T> requestEntity = new HttpEntity<>(Objects.requireNonNull(body));
+        HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders());
         ResponseEntity<Object> serviceResponse;
         try {
             if (parameters != null) {
@@ -43,5 +42,12 @@ public class BaseClient {
             return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsString());
         }
         return serviceResponse;
+    }
+
+    private HttpHeaders defaultHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        return headers;
     }
 }
