4dccb2ab77485fd985744820ab932aba4e3c601b
sergey <alles1028@gmail.com>
2022-11-06 17:20:21 +0300

    Добавлено исключение в обработчик

--- main-service/src/main/java/ru/practicum/explore/exception/ErrorHandler.java
+++ main-service/src/main/java/ru/practicum/explore/exception/ErrorHandler.java
@@ -39,7 +39,8 @@ public class ErrorHandler {
             UnavailableOperationException.class,
             WrongDateException.class,
             WrongOwnerException.class,
-            WrongStateException.class
+            WrongStateException.class,
+            UserIsNotParticipantException.class
     })
     public ResponseEntity<ApiError> handleBadRequest(RuntimeException e) {
         log.info("Ошибка {}: {}", e.getClass().getSimpleName(), e.getMessage());
